c8f1da426795b8c1abe202ddcf9c53ed45a4eb37
Soumya Garg <58630077+soumya-2k01@users.noreply.github.com>
2022-05-04 13:04:10 +0530

    Update SetMatrixZeroes.cpp

--- Day1/SetMatrixZeroes.cpp
+++ Day1/SetMatrixZeroes.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<pair<int,int>> ind;
+        for(int i =0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(matrix[i][j]==0){
+                    ind.push_back({i,j});
+            
+                }
+            }   
+        }
+        for(auto it: ind){
+            int r = it.first, c = it.second;
+            for(int i=0; i<col; i++){
+                matrix[r][i] = 0;
+            }
+            for(int i=0; i<row; i++){
+                matrix[i][c] = 0;
+            }
+        }
+        
+    }
+};
